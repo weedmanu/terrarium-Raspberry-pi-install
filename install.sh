5532d6539f265f15143d955473b52ab36d499d70
weedmanu <terraspi13@gmail.com>
2016-11-03 08:45:28 +0100

    modifié :         install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ""
 RED='\033[1;31m'
-GREEN='\033[0;32m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' 
@@ -215,7 +215,7 @@ if [ "$ouinon" = "y" ] || [ "$ouinon" = "Y" ]; then
 	echo "   http://${ip}/terraspi/admin/"
 	printf "%b\n" "${NC}"
 	echo ""
-	printf "%b\n" "${GREEN}powered${NC}${YELLOW} by ${NC}${LRED}weedmanu${NC}\n"
+	printf "%b\n" "${GREEN}powered${NC}${YELLOW} by ${NC}${RED}weedmanu${NC}\n"
 else
 	printf "%b\n" "${RED}Installation annulée${NC}"
 fi
