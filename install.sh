2e7ebbe8bc97328962cd718ba19f24e99e6fd7fa
manu <manu@localhost.localdomain>
2016-11-15 20:02:22 +0100

    modifié :         README.md 	modifié :         install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
-NEWT_COLORS=' 
-  root=,black
-  window=green,green
-  border=yellow,yellow
-  shadow=red,red
-  textbox=black,green
-  button=black,green
-' \
-echo ""
+CYAN='\033[1;36m'
 RED='\033[1;31m'
-GREEN='\033[0;32m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' 
-echo""
 if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?" --yes-button "oui" --no-button "non" 10 60) then
 	echo ""
-	printf "%b\n" "   ${GREEN}////////////////////////////////////////////////${NC}\n   ${YELLOW}//      Début du programme dinstallation      //${NC}\n   ${RED}////////////////////////////////////////////////${NC}\n"
+	printf "%b\n" "   ${GREEN}////////////////////////////////////////////////\n   ${YELLOW}//      Début du programme dinstallation      //\n   ${RED}////////////////////////////////////////////////${NC}\n"
 	echo ""
 	echo ""
 	printf "%b\n" "${BLUE}     ********************************\n     *   mise à jour du Raspberry   *\n     ********************************${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	apt-get update && apt-get upgrade -y
 	echo ""
 	echo ""
 	printf "%b\n" "${BLUE}     *************************************************\n     *   installation de build, python, git et pip   *\n     *************************************************${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	apt-get install apt-transport-https -y && apt-get install build-essential python-dev python-openssl git python-pip -y
 	echo ""
 	printf "%b\n" "${BLUE}     ***************************************************\n     *   installation de la librairie python 'ephem'   *\n     ***************************************************${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	pip install ephem 
 	echo ""
 	printf "%b\n" "${BLUE}     *****************************************************************\n     *   installation des librairies adafruit pour lire les sondes   *\n     *****************************************************************${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	cd /home/pi
 	git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 	cd Adafruit_Python_DHT
@@ -41,6 +36,7 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	echo ""
 	printf "%b\n" "${BLUE}     ********************************************************************\n     *   installation des librairies pour communiquer avec lécran LCD  *\n     ********************************************************************${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	cd /home/pi
 	git clone https://github.com/dbrgn/RPLCD
 	cd RPLCD
@@ -48,15 +44,15 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	echo ""
 	printf "%b\n" "${BLUE}     ************************************************************\n     *   installation de LAMP (linux apache mysql phpmyadmin)   *\n     ************************************************************${NC}\n"
 	echo ""
-	printf "Vous allez devoir ici ${RED}définir un mot de passe root mysql${NC},\npuis pour ${GREEN}phpmyadmin choisissez apache${NC}\n${RED}définissez un mot de passe pour phpmyadmin${NC} (${YELLOW}mettre le même que mysql cest plus simple${NC})\n"
+	printf "${YELLOW}Vous allez devoir ici définir un mot de passe root mysql,\npuis pour phpmyadmin choisissez apache$\ndéfinissez un mot de passe pour phpmyadmin (mettre le même que mysql cest plus simple${NC})\n"
 	echo ""
-	echo "appuiez sur entrée pour continuer"
+	echo "appuiez sur la touche entrée pour continuer"
 	read a
+	printf "%b\n" "${CYAN}"
 	apt-get install mysql-server python-mysqldb apache2 php5 libapache2-mod-php5 php5-mysql phpmyadmin shellinabox -y
 	echo ""
 	printf "%b\n" "${BLUE}     *************************************\n     *   création de la base de donnée   *\n     *************************************${NC}\n"
 	echo ""
-	read a
 	dbname="Terrarium"
 	echo""		
     mdproot=$(whiptail --title "Mot de passe root Mysql" --passwordbox "Entrer le mot de passe root mysql :" 10 60 3>&1 1>&2 2>&3) 
@@ -65,15 +61,18 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 		mysql -uroot -p${mdproot} -e "CREATE DATABASE ${dbname};"    
 	else
 		echo "Annulé"
-	fi	
-	echo""
+	fi
+	echo ""	
 	printf "${BLUE} liste des base de donnée de mysql, la base Terrarium doit être présente${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	mysql -uroot -p${mdproot} -e "show databases;"
 	echo ""
 	loginbdd=$(whiptail --title "Login user mysql" --inputbox "Choisir un non d'utilisateur pour mysql :" 10 60 3>&1 1>&2 2>&3)
 	exitstatus2=$?
-	if [ $exitstatus2 = 1 ]; then 
+	if [ $exitstatus2 = 0 ]; then 
+		echo "ok"
+	else
 		echo "Annulé"
 	fi
 	echo ""	
@@ -85,13 +84,7 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 		mysql -hlocalhost -uroot -p${mdproot} -e "FLUSH PRIVILEGES;"
 	else
 		echo "Annulé"
-	fi
-	echo ""
-	echo "Création du nouvel utilisateur et donne les droits sur la base de donnée Terrarium"
-	echo ""
-	mysql -hlocalhost -uroot -p${mdproot} -e "CREATE USER ${loginbdd}@localhost IDENTIFIED BY '${mdpbdd}';"
-	mysql -hlocalhost -uroot -p${mdproot} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${loginbdd}'@'localhost';"
-	mysql -hlocalhost -uroot -p${mdproot} -e "FLUSH PRIVILEGES;"
+	fi	
 	echo ""	
 	echo "On crée la table capteurdata"
 	mysql -u${loginbdd} -p${mdpbdd} -hlocalhost -D${dbname} -e "CREATE TABLE capteurdata (dateandtime DATETIME, tempF DOUBLE, humF DOUBLE, tempC DOUBLE, humC DOUBLE);"
@@ -104,8 +97,9 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	echo ""
 	/etc/init.d/mysql restart
 	echo ""
-	printf "%b\n" "${BLUE}   ****************************************************\n   *   téléchargement et installation de terraspiV2   *\n   ****************************************************${NC}\n"
+	printf "%b\n" "${BLUE}   *******************************************************************\n   *   téléchargement et installation de terrarium-Raspberry-pi   *\n   *******************************************************************${NC}\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	cd /var/www/html/
 	rm index.html
 	rm -R terraspi	
@@ -113,17 +107,16 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	chmod -R 770 /var/www/html/
 	echo ""
 	cd /home/pi
-	git clone https://github.com/weedmanu/terraspiV2.git
-	cd /home/pi/terraspiV2
+	git clone https://github.com/weedmanu/terrarium-Raspberry-pi.git
+	cd /home/pi/terrarium-Raspberry-pi
 	mv terraspi -t /var/www/html/
 	chown -R www-data:pi /var/www/html/
 	chmod -R 770 /var/www/html/
-	cd /var/www/html/terraspi/
-	cp install.sh -t /var/www/html/terraspi/prog/
 	cd /var/www/html/terraspi/csv/
 	echo ""
-	printf "%b\n" "${BLUE}    **************\n    *    MySQL   *\n    **************${NC}\n"
+	printf "%b\n" "${BLUE}    **************\n    *    MySQL   *\n    **************\n"
 	echo ""
+	printf "%b\n" "${CYAN}"
 	echo "login mysql"
 	echo ""	
 	sed -i "s/loginbdd/${loginbdd}/g" bdd.json
@@ -138,12 +131,16 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	echo ""
 	loginadmin=$(whiptail --title "Configuration" --inputbox "Choisir un non d'utilisateur pour la page admin :" 10 60 3>&1 1>&2 2>&3)
 	exitstatus4=$?
-	if [ $exitstatus4 = 1 ]; then 
+	if [ $exitstatus4 = 0 ]; then 
+		echo "ok"
+	else
 		echo "Annulé"
 	fi
-	mdpbadmin=$(whiptail --title "Configuration" --passwordbox "Choisir un mot de passe pour cet utilisateur :" 10 60 3>&1 1>&2 2>&3) 
+	mdpadmin=$(whiptail --title "Configuration" --passwordbox "Choisir un mot de passe pour cet utilisateur :" 10 60 3>&1 1>&2 2>&3) 
 	exitstatus5=$?
-	if [ $exitstatus5 = 1 ]; then
+	if [ $exitstatus5 = 0 ]; then
+		echo "ok"
+	else
 		echo "Annulé"
 	fi
 	echo ""	
@@ -171,13 +168,15 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	if [ $exitstatus6 = 1 ]; then 
 		echo "Annulé"
 	fi
+
 	until valid_ip $ip
 	do
 		ip=$(whiptail --title "Configuration" --inputbox "Une adresse ip valide !!! :" 10 60 3>&1 1>&2 2>&3)
 		exitstatus7=$?
 		if [ $exitstatus7 = 1 ]; then 
 			echo "Annulé"
-		fi		
+		fi
+		
 	done
 	echo ""	
 	echo ""
@@ -186,10 +185,8 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	echo ""
 	mysql -uroot -p${mdproot} -hlocalhost -D${dbname} -e "INSERT INTO config (dateetheure, loginadmin, mdpadmin, ip) VALUES ( '$dateetheure', '$loginadmin', '$mdpadmin', '$ip' )";
 	echo ""
-	cd /var/www/html/terraspi/
-	rm install.sh
 	cd /home/pi/
-	rm -R terraspiV2
+	rm -R terrarium-Raspberry-pi
 	crontab -upi -l > tachecron
 	echo "* * * * * python /var/www/html//terraspi/prog/terra.py > /dev/null 2>&1" >> tachecron
 	echo "*/15 * * * * python /var/www/html/terraspi/prog/bdd.py > /dev/null 2>&1" >> tachecron
@@ -201,29 +198,30 @@ if (whiptail --title "Installation" --yesno "voulez vous lancer l'installation ?
 	echo "" >> test
 	echo "exit 0" >> test
 	mv test /etc/rc.local
-	rm test
 	python /var/www/html/terraspi/prog/lcd.py &
 	echo ""
-	printf "%b\n" "${GREEN}           ********************************\n          ********************************${NC}\n${YELLOW}           **    FIN de l installation   **${NC}\n${RED}           ********************************\n           ********************************${NC}\n"
+	printf "%b\n" "${GREEN}           ********************************\n           ********************************\n${YELLOW}           **    FIN de l installation   **\n${RED}           ********************************\n           ********************************${NC}\n"
 	echo ""
 	echo "ensuite :"
 	printf "%b\n" "${BLUE}"
 	printf "           http://${ip}:4200"
 	printf "%b\n" "${NC}"
 	printf "%b\n" "${YELLOW}"
-	echo " Ouvrer ce lien dans votre navigateur , il va passer en https , il faut ajouter une execption de sécuriter en cliquant sur avancé "
-	echo " Cocher conserver de façon permanante, et vous tomberez sur le terminal du pi. fermer la page. "
+	echo " Ouvrez ce lien dans votre navigateur , il va passer en https , il faut ajouter une execption de sécurité en cliquant sur avancé "
+	echo " Cochez conserver de façon permanante, et vous tomberez sur le terminal du pi. fermez la page. "
 	printf "%b\n" "${NC}"
 	echo "Appuyer sur la touche entrée pour continuer"
 	read a
 	printf "%b\n" "${YELLOW}"
-	echo "Ouvrer ce lien dans votre navigateur internet et entrer vos identifiant pour la page admin et régler les derniers paramètres du terrarium"
+	echo "Ouvrez ce lien dans votre navigateur internet et entrez vos identifiant pour la page admin et réglez les derniers paramètres du terrarium"
 	printf "%b\n" "${BLUE}"
 	echo "   http://${ip}/terraspi/admin/"
 	printf "%b\n" "${NC}"
 	echo ""
-	printf "%b\n" "${GREEN}powered${NC}${YELLOW} by ${NC}${LRED}weedmanu${NC}\n"
+	printf "%b\n" "${GREEN}powered ${YELLOW}by ${RED}weedmanu${NC}\n"
+	echo ""
 else
 	whiptail --title "Installation" --msgbox "Installation annulée !!!" 10 60
 fi
+echo ""
 exit
